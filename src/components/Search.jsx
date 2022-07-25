ecb5143690f049ff0c6104e0473dbfca1321e94f
Enes Akkose <ens.akkose@gmail.com>
2022-07-25 18:24:28 +0300

    searchbar edited

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { FiSearch } from 'react-icons/fi'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 import './Search.scss'
 
 function Search() {
   return (
     <form className='header__container__search'>
-        <FiSearch/>
+        <span className='search-icon'>
+          <FiSearch />
+        </span>
+        
         <input type="text" placeholder='Search'/>
+        <span className='close-icon'>
+          <AiOutlineCloseCircle />
+        </span>
+        
     </form>
   )
 }
